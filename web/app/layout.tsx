908756d7916ebceb5b375debf13d0e7bd476824a
Becker <bhocken91@gmail.com>
2026-09-16 08:06:11 -0700

    got zoom out to see my greatness

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -31,6 +31,8 @@ export const viewport: Viewport = {
   themeColor: "#000000",
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
   interactiveWidget: "resizes-content",
 };
