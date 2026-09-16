c0243ddb558b58bde59df55ebb46f57f191eb0a4
Becker <bhocken91@gmail.com>
2026-09-16 15:36:14 -0700

    bottom fixed

--- web/components/shared/use-body-scroll-lock.ts
+++ web/components/shared/use-body-scroll-lock.ts
@@ -12,26 +12,14 @@ export function useBodyScrollLock(isLocked: boolean) {
     const originalStyles = {
       htmlOverflow: html.style.overflow,
       bodyOverflow: body.style.overflow,
-      bodyPosition: body.style.position,
-      bodyTop: body.style.top,
-      bodyLeft: body.style.left,
-      bodyWidth: body.style.width,
     };
 
     html.style.overflow = "hidden";
     body.style.overflow = "hidden";
-    body.style.position = "fixed";
-    body.style.top = `-${scrollY}px`;
-    body.style.left = "0";
-    body.style.width = "100%";
 
     return () => {
       html.style.overflow = originalStyles.htmlOverflow;
       body.style.overflow = originalStyles.bodyOverflow;
-      body.style.position = originalStyles.bodyPosition;
-      body.style.top = originalStyles.bodyTop;
-      body.style.left = originalStyles.bodyLeft;
-      body.style.width = originalStyles.bodyWidth;
       window.scrollTo(0, scrollY);
     };
   }, [isLocked]);
